3119138887e84672fa7270adaad5c020fb0a91c0
Vanessa Cuevas <73256210+vanessancuevas@users.noreply.github.com>
2025-12-02 16:21:08 -0800

    Chapter 0 - Example 0.2 - A Random-Number Distribution

--- sketch.js
+++ sketch.js
@@ -34,17 +34,11 @@ class Walker {
     }
 
     step () {
-        let choice = floor(random(4));
+        let xstep = floor(random(3)) - 1;
+        let ystep = floor(random(3)) - 1;
 
-        if (choice === 0) {
-            this.x++;
-        } else if ( choice === 1) {
-            this.x--;
-        } else if (choice === 2) {
-            this.y++;
-        } else {
-            this.y--;
-        }
+        this.x += xstep;
+        this.y += ystep;
 
     }
 
